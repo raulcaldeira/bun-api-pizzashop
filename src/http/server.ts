27af3276ecd90bae9d0dd77a3a1f28cf144360a0
Raul Santana <ti.raul77@gmail.com>
2024-07-26 14:56:14 -0300

    feat: implement register-restaurant route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,8 @@
-import Elysia from 'elysia'
+import { Elysia } from 'elysia'
+import { registerRestaurante } from './routes/register-restaurante'
 
-const app = new Elysia().get('/', () => {
-  return 'Hello World'
-})
+const app = new Elysia().use(registerRestaurante)
 
 app.listen(3333, () => {
-  console.log('🔥 HTTP server running')
+  console.log('🔥 HTTP server running!')
 })
