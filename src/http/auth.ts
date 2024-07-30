be0154074edc8f99cda1c5a9c22d123b96c01589
Raul Santana <ti.raul77@gmail.com>
2024-07-30 19:53:14 -0300

    feat: add derive in auth

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -1,13 +1,34 @@
-import { Elysia, t } from 'elysia'
+import { Elysia, t, type Static } from 'elysia'
 import jwt from '@elysiajs/jwt'
 import { env } from '../env'
 
-export const auth = new Elysia().use(
-  jwt({
-    secret: env.JWT_SECRET_KEY,
-    schema: t.Object({
-      sub: t.String(),
-      restaurantId: t.Optional(t.String()),
+const jwtPayload = t.Object({
+  sub: t.String(),
+  restaurantId: t.Optional(t.String()),
+})
+
+export const auth = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: jwtPayload,
     }),
-  }),
-)
+  )
+  .derive({ as: 'global' }, ({ cookie, jwt }) => {
+    return {
+      signUser: async (payload: Static<typeof jwtPayload>) => {
+        const token = await jwt.sign(payload)
+
+        cookie.auth.set({
+          value: token,
+          httpOnly: true,
+          maxAge: 60 * 60 * 24 * 7, // 7 days
+          path: '/',
+        })
+      },
+      signOut: () => {
+        // removeCookie('auth')
+        cookie.auth.remove()
+      },
+    }
+  })
--- src/http/routes/authenticate-from-link.ts
+++ src/http/routes/authenticate-from-link.ts
@@ -7,7 +7,7 @@ import { eq } from 'drizzle-orm'
 
 export const autheticateFromLink = new Elysia().use(auth).get(
   '/auth-links/authenticate',
-  async ({ query, jwt, cookie, redirect }) => {
+  async ({ query, redirect, signUser }) => {
     const { code, redirectUrl } = query
     const authLinkFromCode = await db.query.authLinks.findFirst({
       where(fields, { eq }) {
@@ -28,23 +28,28 @@ export const autheticateFromLink = new Elysia().use(auth).get(
       throw new Error('Auth Link expired, please generate a new one.')
     }
 
-    const manageRestaurant = await db.query.restaurants.findFirst({
+    const managedRestaurant = await db.query.restaurants.findFirst({
       where(fields, { eq }) {
         return eq(fields.managerId, authLinkFromCode.userId)
       },
     })
 
-    const token = await jwt.sign({
+    signUser({
       sub: authLinkFromCode.userId,
-      restaurantId: manageRestaurant?.id,
+      restaurantId: managedRestaurant?.id,
     })
 
-    cookie.auth.set({
-      value: token,
-      httpOnly: true,
-      maxAge: 60 * 60 * 24 * 7, // 7 days
-      path: '/',
-    })
+    // const token = await jwt.sign({
+    //   sub: authLinkFromCode.userId,
+    //   restaurantId: manageRestaurant?.id,
+    // })
+
+    // cookie.auth.set({
+    //   value: token,
+    //   httpOnly: true,
+    //   maxAge: 60 * 60 * 24 * 7, // 7 days
+    //   path: '/',
+    // })
 
     await db.delete(authLinks).where(eq(authLinks.code, code))
 
