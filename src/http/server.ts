32c54f3ae88735471cebfd095101dbd6ec15261c
Raul Santana <ti.raul77@gmail.com>
2024-07-29 22:57:02 -0300

    feat: add setup jwt and cookie in Elysia

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,23 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
 import { registerRestaurante } from './routes/register-restaurante'
 import { sendAuthLink } from './routes/send-auth-link'
+import jwt from '@elysiajs/jwt'
+import { env } from '../env'
+import cookie from '@elysiajs/cookie'
 
-const app = new Elysia().use(registerRestaurante).use(sendAuthLink)
+const app = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: t.Object({
+        sub: t.String(),
+        restaurantId: t.Optional(t.String()),
+      }),
+    }),
+  )
+  .use(cookie())
+  .use(registerRestaurante)
+  .use(sendAuthLink)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running!')
