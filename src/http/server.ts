80cdc8cbcf43cdb12046d3f930c24d2835de5053
Raul Santana <ti.raul77@gmail.com>
2024-07-29 22:41:27 -0300

    feat: create the initial structure for the send-auth-link route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,8 @@
 import { Elysia } from 'elysia'
 import { registerRestaurante } from './routes/register-restaurante'
+import { sendAuthLink } from './routes/send-auth-link'
 
-const app = new Elysia().use(registerRestaurante)
+const app = new Elysia().use(registerRestaurante).use(sendAuthLink)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running!')
