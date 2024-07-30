8e3bebd506bd11088f14f2023c98975f84f18765
Raul Santana <ti.raul77@gmail.com>
2024-07-30 20:44:55 -0300

    feat: add get profile route

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,12 +3,14 @@ import { registerRestaurante } from './routes/register-restaurante'
 import { sendAuthLink } from './routes/send-auth-link'
 import { autheticateFromLink } from './routes/authenticate-from-link'
 import { signOut } from './routes/sign-out'
+import { getProfile } from './routes/get-profile'
 
 const app = new Elysia()
   .use(registerRestaurante)
   .use(sendAuthLink)
   .use(autheticateFromLink)
   .use(signOut)
+  .use(getProfile)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running!')
