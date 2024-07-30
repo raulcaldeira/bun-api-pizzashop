77f04d0d09a91ec48d2792658efd3aa0f6a9ab88
Raul Santana <ti.raul77@gmail.com>
2024-07-30 19:53:56 -0300

    feat: add signOut route

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,11 +2,13 @@ import { Elysia } from 'elysia'
 import { registerRestaurante } from './routes/register-restaurante'
 import { sendAuthLink } from './routes/send-auth-link'
 import { autheticateFromLink } from './routes/authenticate-from-link'
+import { signOut } from './routes/sign-out'
 
 const app = new Elysia()
   .use(registerRestaurante)
   .use(sendAuthLink)
   .use(autheticateFromLink)
+  .use(signOut)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running!')
