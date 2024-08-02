2a61eb1056f97bf1e39495a4956203d88990248d
Raul Santana <ti.raul77@gmail.com>
2024-08-02 16:41:40 -0300

    feat: add deliver order route

---
+++ src/http/routes/deliver-order.ts
@@ -0,0 +1,60 @@
+import Elysia, { t } from 'elysia'
+import { auth } from '../auth'
+import { UnauthorizedError } from '../errors/unauthorized-error'
+import { db } from '../../db/connection'
+import { orders } from '../../db/schema'
+import { eq } from 'drizzle-orm'
+
+export const deliverOrder = new Elysia().use(auth).patch(
+  '/orders/:orderId/deliver',
+  async ({ getCurrentUser, set, params }) => {
+    const { orderId } = params
+    const { restaurantId } = await getCurrentUser()
+
+    if (!restaurantId) {
+      throw new UnauthorizedError()
+    }
+
+    const order = await db.query.orders.findFirst({
+      with: {
+        restaurant: {
+          columns: {
+            id: true,
+          },
+        },
+      },
+      where(fields, { eq }) {
+        return eq(fields.id, orderId)
+      },
+    })
+
+    if (!order) {
+      set.status = 400
+
+      return { message: 'Order not found.' }
+    }
+
+    if (order.restaurant.id !== restaurantId) {
+      throw new UnauthorizedError()
+    }
+
+    if (order.status !== 'delivering') {
+      set.status = 400
+
+      return {
+        message:
+          'You cannot deliver orders that are not in "delivering" status.',
+      }
+    }
+
+    await db
+      .update(orders)
+      .set({ status: 'delivered' })
+      .where(eq(orders.id, orderId))
+  },
+  {
+    params: t.Object({
+      orderId: t.String(),
+    }),
+  },
+)
--- src/http/server.ts
+++ src/http/server.ts
@@ -8,6 +8,7 @@ import { getManagedRestaurant } from './routes/get-managed-restaurant'
 import { getOrderDetails } from './routes/get-order-details'
 import { approveOrder } from './routes/approve-order'
 import { cancelOrder } from './routes/cancel-order'
+import { deliverOrder } from './routes/deliver-order'
 
 const app = new Elysia()
   .use(registerRestaurante)
@@ -19,6 +20,7 @@ const app = new Elysia()
   .use(getOrderDetails)
   .use(approveOrder)
   .use(cancelOrder)
+  .use(deliverOrder)
   .onError(({ code, error, set }) => {
     switch (code) {
       case 'VALIDATION': {
